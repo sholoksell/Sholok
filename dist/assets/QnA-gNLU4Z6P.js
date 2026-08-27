13fa0fd40aedc961676b46efc9157dfaf7eb359b
Maksudul <maksudulk021@gmail.com>
2026-08-27 15:38:44 +0600

    Image search: map clothing labels to 'lawn' (matches DB products)

---
+++ dist/assets/QnA-gNLU4Z6P.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C1-zawBK.js";import{H as s}from"./Header-9d8YBsQw.js";const n=()=>r.jsx("div",{className:"min-h-screen bg-background",children:r.jsx(s,{})});export{n as default};
